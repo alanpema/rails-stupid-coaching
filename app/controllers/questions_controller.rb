f5056cb0e73138ba9fe721e00ae8630e341e9dc5
alan peña <alanert04@gmail.com>
2023-12-11 14:07:03 -0600

    challenge complete

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+  def ask
+
+  end
+
+  def answer
+    @question = params[:question]
+    @answer = if @question == "I am going to work"
+      "Great!"
+    elsif @question.include?("?")
+      "Silly question, get dressed and go to work!"
+    else
+      "I don't care, get dressed and go to work!"
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  get 'ask', to: "questions#ask", as: :ask
+
+  get "answer", to: "questions#answer", as: :answer
 end
